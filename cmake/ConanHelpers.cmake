c7e1b64a272d5b63105a760207221a310395a309
Simon Rydell <simonwrydell@gmail.com>
2022-05-21 18:53:12 +0200

    Update to the latest conan.cmake file

--- cmake/ConanHelpers.cmake
+++ cmake/ConanHelpers.cmake
@@ -42,14 +42,10 @@ endmacro(find_conan_packages)
 # Downloads module to run conan from cmake
 function(get_conan_helper)
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(
-      STATUS
-        "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(
-      DOWNLOAD
-      "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.15/conan.cmake"
-      "${CMAKE_BINARY_DIR}/conan.cmake")
-  endif()
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                  "${CMAKE_BINARY_DIR}/conan.cmake"
+                  TLS_VERIFY ON)
   include(${CMAKE_BINARY_DIR}/conan.cmake)
 endfunction(get_conan_helper)
 
